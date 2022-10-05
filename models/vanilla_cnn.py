946077be3fea6c7b74d6e18d7ff20ba921ccb3c5
Hoang Nguyen <nvthoang@gmail.com>
2022-10-06 11:23:53 +1300

    spacing

--- models/vanilla_cnn.py
+++ models/vanilla_cnn.py
@@ -3,8 +3,7 @@ import torch.nn as nn
 
 
 class vanilla_cnn(nn.Module):   
-    def __init__(self, 
-                 in_channels:int=4, out_channels:int=1,num_features:int=64, 
+    def __init__(self, in_channels:int=4, out_channels:int=1,num_features:int=64, 
                  kernel_size:int=3, stride:str=1, padding:str=1, pooling_size:tuple=(1200, 1200),
                  between_blocks:int=2):
         super(vanilla_cnn, self).__init__()
@@ -18,7 +17,6 @@ class vanilla_cnn(nn.Module):
         downs = [cnn_block(nf*(2**(i+1)), nf*(2**(i)), k, s, p, pool_size) for i in reversed(range(0, b))]
         last = [cnn_block(nf, 1, k, s, p, pool_size)]
         all_blocks = first + ups + between + downs + last
-        
         self.model = nn.Sequential(*all_blocks)
         
     def forward(self, x):
@@ -26,12 +24,8 @@ class vanilla_cnn(nn.Module):
 
 
 class cnn_block(nn.Module):   
-    def __init__(self, in_channels:int=4, 
-                 out_channels:int=256, 
-                 kernel_size:int=3, 
-                 stride:str=1, 
-                 padding:str=1,
-                 pooling_size:tuple=(1200, 1200)):
+    def __init__(self, in_channels:int=4, out_channels:int=256, kernel_size:int=3, 
+                 stride:str=1, padding:str=1, pooling_size:tuple=(1200, 1200)):
         super(cnn_block, self).__init__()
         self.model = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
                                    nn.BatchNorm2d(out_channels),
